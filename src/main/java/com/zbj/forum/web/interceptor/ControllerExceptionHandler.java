08d6b820f6295cc5af1f2942ede9015808edd95c
DreamYao <805229888@qq.com>
2017-01-28 22:37:43 +0800

    增加日志记录切面

--- src/main/java/com/zbj/forum/web/interceptor/ControllerExceptionHandler.java
+++ src/main/java/com/zbj/forum/web/interceptor/ControllerExceptionHandler.java
@@ -6,24 +6,24 @@ import com.zbj.forum.web.common.Result;
 import org.aspectj.lang.ProceedingJoinPoint;
 
 /**
- * Created by dreamyao on 2017/1/28.
+ * @author dreamyao
+ *         Created by dreamyao on 2017/1/28.
+ *         controller层异常处理切面
  */
 public class ControllerExceptionHandler {
 
     // 对controller层类进行异常处理的方法
-    public Object ExceptionHandling(ProceedingJoinPoint joinPoint){
+    public Object ExceptionHandling(ProceedingJoinPoint joinPoint) {
         Object result = null;
         try {
-            System.out.println("=====================执行开始==============================");
-            result= joinPoint.proceed();
+            result = joinPoint.proceed();
         } catch (CRUDException e) {
-            result = new Result<>(e.getCode(),e.getMessage());
+            result = new Result<>(e.getCode(), e.getMessage());
         } catch (Exception e) {
             result = new Result<>(ExceptionCode.SYSTEM_ERROR, "系统服务异常!");
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
-        System.out.println("========================执行结束===========================");
         return result;
     }
 }
